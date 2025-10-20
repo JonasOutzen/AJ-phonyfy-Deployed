4f3d788eb32cdf0a9741c3b4405b190b260fd3e3
Asger <asgersh@hotmail.com>
2025-10-20 10:55:14 +0200

    Initial commit

---
+++ src/main/java/app/routes/Routes.java
@@ -0,0 +1,19 @@
+package app.routes;
+
+import io.javalin.apibuilder.EndpointGroup;
+
+import static io.javalin.apibuilder.ApiBuilder.get;
+import static io.javalin.apibuilder.ApiBuilder.path;
+
+public class Routes {
+
+private Sample1Routes sample1Routes = new Sample1Routes();
+private Sample2Routes sample2Routes = new Sample2Routes();
+
+public EndpointGroup getRoutes() {
+    return () -> {
+        path("/sample1", sample1Routes.getRoutes());
+        path("/sample2", sample2Routes.getRoutes());
+    };
+}
+}

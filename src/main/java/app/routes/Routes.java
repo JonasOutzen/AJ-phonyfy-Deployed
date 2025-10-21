25fa2d5527db058ac7e636bd8c0af32176724943
Asger <asgersh@hotmail.com>
2025-10-21 15:36:49 +0200

    Tilføjet entities

--- src/main/java/app/routes/Routes.java
+++ src/main/java/app/routes/Routes.java
@@ -8,12 +8,10 @@ import static io.javalin.apibuilder.ApiBuilder.path;
 public class Routes {
 
 private Sample1Routes sample1Routes = new Sample1Routes();
-private Sample2Routes sample2Routes = new Sample2Routes();
 
 public EndpointGroup getRoutes() {
     return () -> {
         path("/sample1", sample1Routes.getRoutes());
-        path("/sample2", sample2Routes.getRoutes());
     };
 }
 }
